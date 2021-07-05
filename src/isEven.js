ae1bf691ed07a113e28042a06f7766a064df01c2
btea <2356281422@qq.com>
2021-07-05 14:32:13 +0800

    feat: 判断奇数、偶数，生成随机数

---
+++ src/isEven.js
@@ -0,0 +1,13 @@
+import { isOdd } from './isOdd';
+/**
+ * @desc 判断一个数是否为偶数
+ * @param {number|...} val 传入的参数
+ * @return {boolean} 返回是否为偶数
+ */
+export function isEven(val) {
+    if (!isNumber(val)) {
+        console.log(`输入的参数 ${val} 不是number类型`);
+        return;
+    }
+    return !isOdd(val);
+}
---
+++ src/randomNum.js
@@ -0,0 +1,26 @@
+import { isNumber } from './type';
+/**
+ * @desc 生成指定范围的随机数
+ * @param {number} max 范围区间最大数,返回的随机数不包含
+ * @param {number} min 范围区间最小数，返回的随机数包含
+ * @param {boolean} isInteger 是否返回整数
+ * @return {number} 生成的随机数
+ */
+export function randomNum(max, min, isInteger) {
+    if (isNumber(max)) {
+        console.log(`输入的参数 ${max} 不是一个数字`);
+        return;
+    }
+    if (isNumber(min)) {
+        console.log(`输入的参数 ${min} 不是一个数字`);
+        return;
+    }
+    if (max <= min) {
+        console.log(`输入的区间范围 max: ${max} 应该大于 min: ${min}`);
+    }
+    const v = Math.random() * (max - min) + min;
+    if (isInteger) {
+        return Math.floor(v);
+    }
+    return v;
+}
