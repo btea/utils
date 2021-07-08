2f579502da294b3bde56e1b229ec3b427222fed2
btea <2356281422@qq.com>
2021-07-08 17:26:34 +0800

    feat: build

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 export { isLeapyear, getMonthTotal, formatDate } from './src/date';
+export { randomColor } from './src/color';
+export { isEven } from './src/isEven';
+export { isOdd } from './src/isOdd';
+export { randomNum } from './src/randomNum';
+export { isString, isNumber, isArray, isObject, isTruly, getType } from './src/type';
