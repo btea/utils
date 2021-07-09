135289cdf00cf70ab6e3e4b3041b321a477e57c8
btea <2356281422@qq.com>
2021-07-09 11:01:58 +0800

    chore: 配置命令修改

--- build.js
+++ build.js
@@ -1,6 +1,14 @@
-require('esbuild').buildSync({
+const bindEnv = require('./bindEnv');
+const format = process.argv[2] || '';
+let outfile = 'out.js';
+if (format) {
+    outfile = `out.${format}.js`;
+}
+require('esbuild').build({
     entryPoints: ['index.js'],
     bundle: true,
     loader: { '.js': 'jsx' },
-    outfile: 'out.js'
+    define: { require_utils: 'zys_utils' },
+    plugins: [bindEnv],
+    outfile
 });
