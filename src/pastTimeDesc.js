4cb523f237ae9d458c90745750bb744110b812c4
btea <2356281422@qq.com>
2021-07-23 17:15:24 +0800

    feat: time desc

---
+++ src/pastTimeDesc.js
@@ -0,0 +1,40 @@
+import { formatDate } from './date';
+
+const one_hour = 3600 * 1000;
+const one_day = 24 * one_hour;
+/**
+ * @desc 处理过去时间描述
+ * @param {Date|number|string} time 需要判断处理的时间
+ * @return {string} 返回描述的字符串
+ */
+export function pastTimeDesc(time) {
+    time = new Date(time).now();
+    if (time >= getTodayZero()) {
+        return formatDate(time, 'hh:mm');
+    }
+    if (isYesterday(time)) {
+        return '昨天';
+    }
+    // TODO 更多情况判断
+}
+
+/**
+ * @desc 获取今天零点的时间
+ * @return {number} 返回的今天零点的时间戳
+ */
+export function getTodayZero() {
+    const t = new Date();
+    let str = `${t.getFullYear()}-${t.getMonth() + 1}-${t.getDate()}`;
+    return new Date(str).now();
+}
+
+/**
+ * @desc 判断时间是否为昨天
+ * @param {number} time 需要判断的时间
+ * @return {boolean} 返回 true/false
+ */
+export function isYesterday(time) {
+    const zero = getTodayZero();
+    time = new Date(time).now();
+    return time < zero && time >= zero - one_day;
+}
