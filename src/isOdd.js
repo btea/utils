90543e3f0d037efda4d4d308767a588ba8655ad3
btea <2356281422@qq.com>
2024-12-10 10:05:19 +0800

    feat: test

--- src/isOdd.js
+++ src/isOdd.js
@@ -5,6 +5,7 @@ import { isNumber } from './type';
  * @return {boolean} 返回是否为奇数
  */
 export function isOdd(val) {
+    console.log('isOdd');
     if (!isNumber(val)) {
         console.log(`输入的参数 ${val} 不是number类型`);
         return false;
