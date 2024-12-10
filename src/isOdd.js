34257462e1ea6cce711bdea079d6ef4a6e9df3d3
btea <2356281422@qq.com>
2024-12-10 13:47:37 +0800

    chore: add comment

--- src/isOdd.js
+++ src/isOdd.js
@@ -7,6 +7,9 @@ import { isNumber } from './type';
 export function isOdd(val) {
     console.log('reset comment' + ' isOdd');
     console.log('dedupe')
+    console.log('dedupe1')
+    console.log('dedupe2')
+    console.log('dedupe3')
     if (!isNumber(val)) {
         console.log(`输入的参数 ${val} 不是number类型`);
         return false;
