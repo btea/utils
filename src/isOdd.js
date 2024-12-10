aaa2ced450c7b7f60e79eb79b54f6c0b38cb6187
btea <2356281422@qq.com>
2024-12-10 14:20:00 +0800

    chore: remove all comment

--- src/isOdd.js
+++ src/isOdd.js
@@ -5,11 +5,6 @@ import { isNumber } from './type';
  * @return {boolean} 返回是否为奇数
  */
 export function isOdd(val) {
-    console.log('reset comment' + ' isOdd');
-    console.log('dedupe')
-    console.log('dedupe1')
-    console.log('dedupe2')
-    console.log('dedupe3')
     if (!isNumber(val)) {
         console.log(`输入的参数 ${val} 不是number类型`);
         return false;
