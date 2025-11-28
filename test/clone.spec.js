f63f452161ffcda23ccf0320a9373676e70ea3a1
btea <2356281422@qq.com>
2025-11-28 21:22:41 +0800

    chore: udpate

--- test/clone.spec.js
+++ test/clone.spec.js
@@ -1,6 +1,5 @@
-import _ from 'lodash';
 import { deepClone } from '../src/clone';
-import { describe, it, assert } from 'vitest';
+import { describe, it, assert, expect } from 'vitest';
 
 const { equal } = assert;
 
@@ -15,22 +14,17 @@ describe('深度克隆一个对象或者数组', () => {
     let arr = ['one', 'two', { name: 'three' }];
     it('克隆对象obj', () => {
         const _obj = deepClone(obj);
-        const isEqual = _.isEqual(_obj, obj);
-        const notEqual = _obj !== obj;
-        equal(isEqual, true);
-        equal(notEqual, true);
+        expect(_obj).toEqual(obj);
+        expect(_obj).not.toBe(obj);
     });
     it('克隆数组arr', () => {
         const _arr = deepClone(arr);
-        const isEqual = _.isEqual(_arr, arr);
-        const notEqual = _arr !== arr;
-        equal(isEqual, true);
-        equal(notEqual, true);
+        expect(arr).toEqual(_arr);
     });
     it('循环对象', () => {
         const o = {cir: obj}
         obj.newKey = o
         const _o = deepClone(o)
-        equal(_.isEqual(_o, o), true)
+        expect(_o.cir).toEqual(o.cir)
     })
 });
