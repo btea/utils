b694200d19450b4016c2f554a017c91e85f7c277
btea <2356281422@qq.com>
2025-11-30 07:54:51 +0800

    feat: add format parameter (#7)
    * feat: add format parameter
    
    * fix: update

--- src/color.js
+++ src/color.js
@@ -1,7 +1,14 @@
 /**
  * @desc 生成随机颜色
- * @return {string} 返回一个16进制的颜色字符串
+ * @param {string} format 颜色格式，支持 'hex' 和 'rgb'，默认为 'hex'
+ * @return {string} 返回一个16进制的颜色字符串或者RGB颜色字符串
  */
-export function randomColor() {
+export function randomColor(format = 'hex') {
+    if (format === 'rgb') {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        return `rgb(${r}, ${g}, ${b})`;
+    }
     return '#' + Math.random().toString(16).slice(2, 8);
 }
